bc6718fa844a0d8c0f41f9131a86002f2ada3e93
ToanTHCS <huongchuongbc1978@gmail.com>
2025-02-05 15:04:05 +0700

    Update get-api-keys.js

--- api/get-api-keys.js
+++ api/get-api-keys.js
@@ -1,17 +1,12 @@
-// /pages/api/get-api-keys.js (Sử dụng ESM)
-
 export default function handler(req, res) {
     if (req.method === 'GET') {
         const apiKey = process.env.API_GPT;  // Lấy API key từ biến môi trường
-
         if (apiKey) {
-            res.status(200).json({ apiKey });  // Trả về API key dưới dạng JSON
+            return res.status(200).json({ apiKey });
         } else {
-            // Nếu API key không tồn tại trong biến môi trường
-            res.status(400).json({ error: 'API Key không tồn tại trong biến môi trường' });
+            return res.status(400).json({ error: 'API Key không tồn tại trong biến môi trường' });
         }
     } else {
-        // Nếu phương thức không phải GET
-        res.status(405).json({ error: 'Phương thức yêu cầu không được phép' });
+        return res.status(405).json({ error: 'Method Not Allowed' });  // Chỉ hỗ trợ phương thức GET
     }
 }
